5a700c46bac91a3ee34f947edb01a36a234c062a
Bhavya <bhavya123@gmail.com>
2026-02-24 12:29:57 +0530

    Add GitHub Actions for Docker

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
 def greet():
     return "Hello, Version Control Automation using Git and GitHub!"
 
 if __name__ == "__main__":
-    print(greet())
+    app.run(host="0.0.0.0", port=5000)
